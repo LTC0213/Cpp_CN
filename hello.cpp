c5a421a40b5567d1da36bfc7e0855f4431e24981
100ask <user@100ask.com>
2020-11-11 03:48:32 -0500

    first commit

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    std::string str;
+    std::cin >> str;
+    std::cout << "Echo: " << str << std::endl;
+    return 0;
+}
